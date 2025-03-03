054a82fec25667fb77ef5f971b9a808e4cef5940
Basile Gandon <gandon6u@etu.univ-lorraine.fr>
2025-03-03 15:23:03 +0100

    Improve install_env

--- install_env.ps1
+++ install_env.ps1
@@ -1,6 +1,9 @@
 # Create a new conda environment "venv" with Python 3.11.11
 conda create -n venv python=3.11.11 -y
 
+# Install pygimli from the conda-forge channel using the "gimli::pygimli" specification
+conda install -n venv -c conda-forge -y vtk gimli::pygimli
+
 # Load the conda hook to set up conda commands in the session
 & "C:\anaconda3\shell\condabin\conda-hook.ps1"
 
@@ -15,9 +18,6 @@ python -m pip install torch torchvision torchaudio --index-url https://download.
 
 # Install additional dependencies as listed in requirements.txt
 python -m pip install -r requirements.txt
-
-# Install pygimli from the conda-forge channel using the "gimli::pygimli" specification
-conda install -n venv -c conda-forge -y gimli::pygimli
 
 # Create a helper script to activate the conda environment later if needed.
 Set-Content -Path "activate_conda.ps1" -Value "powershell -ExecutionPolicy ByPass -NoExit -Command '& 'C:\anaconda3\shell\condabin\conda-hook.ps1'; conda activate venv'"
